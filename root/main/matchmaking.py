891a6bf41f9edd520a76817a3fed28331bc63b10
Aditya-madwal <madwaladitya@gmail.com>
2024-05-30 23:41:10 +0530

    suggestions added

--- root/main/matchmaking.py
+++ root/main/matchmaking.py
@@ -1,7 +1,6 @@
 import os
 import google.generativeai as genai
 
-os.environ['GOOGLE_API_KEY'] = "AIzaSyDmLQuUp2Rxi0_5U6EtExfERczih72he_c"
 genai.configure(api_key = os.environ['GOOGLE_API_KEY'])
 
 model = genai.GenerativeModel('gemini-pro')
@@ -11,3 +10,12 @@ def get_keywords(input_text) :
 
 	list_of_keywords = response.text.split(',')
 	return list_of_keywords
+
+def right_or_wrong(input_text) :
+	response = model.generate_content(f"check whether the info shared here is an assertive statement and if yes then tell whether it is right or not , answer in yes or no strictly , no other answer is allowed : {input_text}")
+
+	print(response.text)
+
+# right_or_wrong("girls have short hair")
+
+right_or_wrong("using sulphuric acid on skin is better better glow")
